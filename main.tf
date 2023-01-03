a71fd0d6ef0cc75e361ade38e2205b156530723a
Raz Ben Netanel <52315689+raz-bn@users.noreply.github.com>
2023-01-03 11:10:30 +0200

    create bucket (#1)
    atlantis apply

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket = "atlantis-tfstate-test"
+    key = "tfstate"
+    region = "eu-west-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-buckettttttttt"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
