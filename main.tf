d2267e2bc859cf43410204b6f672c395c1963fcb
Poodle <52315689+raz-bn@users.noreply.github.com>
2023-01-03 10:32:36 +0200

    create bucket

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-buckettttttttt"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
