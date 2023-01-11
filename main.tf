ee024099c13e310e5e55d1faa99ddf499ce5316e
Poodle <52315689+raz-bn@users.noreply.github.com>
2023-01-11 16:52:55 +0200

    testing

--- main.tf
+++ main.tf
@@ -12,17 +12,15 @@ terraform {
   }
 }
 
-
-resource "aws_s3_bucket" "b" {
-  bucket = "atlantis-test-test"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+provider "aws" {
+  region = "eu-west-1"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "aws_instance" "tester" {
+  ami           = "ami-0fe0b2cf0e1f25c8a"
+  instance_type = "t2.small"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
